7dfbc1bf3d301790a5fa8d9f4263bb89295dce57
VenomExE <121004003+ExE-Venom@users.noreply.github.com>
2023-03-04 02:36:22 +0530

    ADDED OPENAI SUPPORT

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
 const Insta = require('./insta.js');
 const client = new Insta.Client();
-const chatbot = require("node-fetch").default;
+const { Configuration, OpenAIApi } = require("openai");
+var reply = "";
+const config = new Configuration({
+    apiKey: process.env.API_KEY,
+  });
+
+const openai = new OpenAIApi(config);
 
 client.on('connected', () => {
     console.log(`${client.user.username} Is Ready Now For Chats`);
 });
 
-client.on('messageCreate', (message) => {
+client.on('messageCreate', async(message) => {
     if (message.author.id === client.user.id) return
     message.markSeen();
 
-    if(message.content.toLowerCase().includes('hi')){ 
+    if(message.content.toLowerCase().includes('hi') || message.content.toLowerCase().includes('hello')){ 
         return message.chat.sendMessage('VENOM IS MY DEVELOPER CHECK OUT HIS CHANNEL :- https://youtube.com/c/VenomExE');
     } else
-    chatbot(`https://brv-chat.vercel.app/api?message=${encodeURIComponent(message.content)}`)
-    .then(res => res.json())
-    .then(json => {
-      message.chat.sendMessage(json.reply);
-    }).catch(err => {});
+    reply = await openai.createChatCompletion({
+  model: "gpt-3.5-turbo",
+  messages: [{role: "user", content: message}],
+});
+message.chat.sendMessage(reply.data.choices[0].message);
 });
 
-client.login('<insta-username>', '<insta-password>');
+client.login(process.env.USERNAME, process.env.PASSWORD);
